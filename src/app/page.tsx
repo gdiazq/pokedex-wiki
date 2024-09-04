638d9f5d6b79aa7847bebe5238a8c1f2291d24e6
Guillermo Diaz <guillermoe.diazq@gmail.com>
2024-09-04 01:56:23 -0400

    La navbar sera esta mientras

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
 // Objective: Create the main page of the app
 'use client'
 
-import Header from '@/components/layout/Header'
+import Navbar  from '@/components/layout/Navbar'
 
 
 export default function Home() {
   return (
     <>
-      <Header />
+      <Navbar />
       <main className="p-4">
         <div className="space-y-6 py-24 text-center px-4">
           <h1 className="text-5xl font-semibold text-black dark:text-white drop-shadow-md">
--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,14 +1,66 @@
 'use client';
 
+import React from "react";
+import {Navbar, NavbarBrand, NavbarContent, NavbarItem, NavbarMenuToggle, NavbarMenu, NavbarMenuItem, Link} from "@nextui-org/react";
+import Image from 'next/image';
 import { ThemeSwitch } from "@/components/providers/ThemeSwitch";
 
-const Navbar = () => {
+export default function App() {
+    const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+
+    const menuItems = [
+        "Features",
+        "Customers",
+        "Integrations"
+    ];
 
     return (
-        <nav className="flex items-center justify-center gap-x-5">
-            <ThemeSwitch />
-        </nav>
+        <Navbar className="w-full" onMenuOpenChange={setIsMenuOpen}>
+            <NavbarContent>
+                <NavbarMenuToggle
+                    aria-label={isMenuOpen ? "Close menu" : "Open menu"}
+                    className="sm:hidden"
+                />
+                <NavbarBrand>
+                    <Image className="font-bold text-inherit" src="/logo.png" width={80} height={80} priority={true} alt="logo" />
+                </NavbarBrand>
+            </NavbarContent>
+
+            <NavbarContent className="hidden sm:flex gap-4" justify="center">
+                <NavbarItem>
+                    <Link color="foreground" href="#">
+                        Features
+                    </Link>
+                </NavbarItem>
+                <NavbarItem>
+                    <Link color="foreground" href="#">
+                        Customers
+                    </Link>
+                </NavbarItem>
+                <NavbarItem>
+                    <Link color="foreground" href="#">
+                        Integrations
+                    </Link>
+                </NavbarItem>
+            </NavbarContent>
+            
+            <NavbarContent justify="end">
+                <ThemeSwitch />
+            </NavbarContent>
+
+            <NavbarMenu>
+                {menuItems.map((item, index) => (
+                    <NavbarMenuItem key={`${item}-${index}`}>
+                        <Link
+                            className="w-full"
+                            href="#"
+                            size="lg"
+                        >
+                            {item}
+                        </Link>
+                    </NavbarMenuItem>
+                ))}
+            </NavbarMenu>
+        </Navbar>
     );
 }
-
-export default Navbar;
