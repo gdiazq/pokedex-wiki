c69f9c96fdc43fe4b7bc45e39be1b93444ca89b4
Guillermo Diaz <guillermoe.diazq@gmail.com>
2024-09-14 00:45:34 -0300

    generando las card para paginado en la pagina principal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,10 +6,6 @@ import { BeatLoader } from "react-spinners"
 import Navbar  from '@/components/layout/Navbar'
 import { fetchAllPokemon } from '@/api/fetchAllPokemon/fetchAllPokemon'
 
-interface Pokemon {
-  name: string;
-}
-
 interface PokemonDetails {
   id: number;
   name: string;
@@ -35,8 +31,8 @@ interface PokemonDetails {
   }>;
 }
 
-export default function Home({ initialPokemon }: { initialPokemon: Pokemon[] | null }) {
-  const [pokemon, setPokemon] = useState<Pokemon[]>(initialPokemon || []);
+export default function Home({ initialPokemon }: { initialPokemon: PokemonDetails[] | null }) {
+  const [pokemon, setPokemon] = useState<PokemonDetails[]>(initialPokemon || []);
   const [loading, setLoading] = useState<boolean>(!initialPokemon);
   const [error, setError] = useState<string | null>(null);
 
@@ -70,14 +66,27 @@ export default function Home({ initialPokemon }: { initialPokemon: Pokemon[] | n
             { loading ? (
                <BeatLoader />
             ) : (
-              <ul>
-                {pokemon.map((p, index) => (
-                  <li key={index}>
-                    {p.name.charAt(0).toUpperCase() + p.name.slice(1)}
-                  </li>
-                ))}
-              </ul>
-            )}
+              <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+              {pokemon.map((p) => (
+                <div key={p.id} className="bg-white dark:bg-gray-800 shadow-md rounded-lg p-4">
+                  <img 
+                    className="mx-auto w-32 h-32"
+                    src={p.sprites.other.dream_world.front_default || p.sprites.front_default} 
+                    alt={p.name}
+                  />
+                  <h2 className="text-lg font-bold text-gray-800 dark:text-white capitalize">{p.name}</h2>
+                  <p className="text-gray-600 dark:text-gray-300">
+                    Tipo: {p.types.map(t => t.type.name).join(', ')}
+                  </p>
+                  <p className="text-gray-600 dark:text-gray-300">Peso: {p.weight / 10} kg</p>
+                  <p className="text-gray-600 dark:text-gray-300">Altura: {p.height / 10} m</p>
+                  <p className="text-gray-600 dark:text-gray-300">
+                    Habilidades: {p.abilities.map(a => a.ability.name).join(', ')}
+                  </p>
+                </div>
+              ))}
+            </div>
+          )}
           </div>
         </div>
       </main>
